9e84d17105bafb6efbee9c24f32aa0a72d88d83f
dataguy7777 <166624205+dataguy7777@users.noreply.github.com>
2024-12-06 09:28:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.write(
-        f'<iframe src="https://bird.ecb.europa.eu/view/Framework/1544692898369?published=true"></iframe>',
-        unsafe_allow_html=True,
-    )
+# Set the page configuration to maximize window size
+st.set_page_config(page_title="Large Webpage Embed", layout="wide")
+
+st.title("Large Webpage Embed Example")
+
+# Define the URL to embed
+url = "https://bird.ecb.europa.eu/view/Framework/1544692898369?published=true"
+
+# Add the iframe to the page with adjusted width and height
+components.html(
+    f"""
+    <iframe src="{url}" width="100%" height="800px" frameborder="0"></iframe>
+    """,
+    height=800,  # Set the height of the iframe
+)
