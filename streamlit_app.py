2ecd7c57cb0ae1acc8b1204d32df0273db5c3bd0
dataguy7777 <166624205+dataguy7777@users.noreply.github.com>
2024-12-06 09:25:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    f'<iframe src="https://bird.ecb.europa.eu/view/Framework/1544692898369?published=true",
+    f'<iframe src="https://bird.ecb.europa.eu/view/Framework/1544692898369?published=true',
         unsafe_allow_html=True,
     )
